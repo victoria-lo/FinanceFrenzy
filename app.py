7bbe20f5028bce2d8022c39d21a49b9e75ff9b54
Steffy Lo <steffy@bambu.co>
2021-06-19 10:39:30 +0800

    added game timer

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QApplication, QWidget
-import sys
 from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import QTimer
+import sys
 
 
 class Window(QWidget):
@@ -8,6 +9,19 @@ class Window(QWidget):
         super().__init__()
         self.setWindowTitle("Financial Times")
         self.setWindowIcon(QIcon("./assets/financial-times.png"))
+
+        self.day = 0
+        self.timer = QTimer()
+        self.timer.setInterval(3000)
+        self.timer.timeout.connect(self.a_day_has_passed)
+        self.timer.start()
+
+    def a_day_has_passed(self):
+        self.day += 1
+        print("A day has passed...")
+        if self.day > 365 * 5 + 1:
+            self.timer.stop()
+            print("GAME OVER!")
 
 
 app = QApplication(sys.argv)
